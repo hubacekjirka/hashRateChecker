64d1d8013d84b01b34304410896cad34f9273bcf
Jiri Hubacek <hubacekjirka@gmail.com>
2019-02-13 01:04:52 +1100

    initial commit

---
+++ hashRateCheck/config_example.py
@@ -0,0 +1,11 @@
+import os.path
+from pathlib import Path
+
+
+# current folder + other folders (optional) + mask
+sourceFiles=f'/FILEPATH GOES HERE/*.txt'
+
+smtpUser = 'XXXX@gmail.com'
+smtpPasswd = 'YYYY'
+sender = 'XXXX@gmail.com'
+recipients = 'ZZZZ@gmail.com'
